2a90d3bcd229097aa35121a36df2b2bd817b3fe4
Hantonny Korrea <hantonny.20@gmail.com>
2020-10-26 23:04:59 -0400

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,82 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => (
-  <Title>Explore repositórios no Github</Title>
+  <>
+    <img src={logoImg} alt="GitHubExplorer" />
+    <Title>Explore repositórios no Github</Title>
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
+          alt="Diego Fernandes"
+        />
+        <div>
+          <strong>
+            rocketseat/unform
+          </strong>
+          <p>
+            Performance-focused API for React forms rocket
+          </p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
+          alt="Diego Fernandes"
+        />
+        <div>
+          <strong>
+            rocketseat/unform
+          </strong>
+          <p>
+            Performance-focused API for React forms rocket
+          </p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
+          alt="Diego Fernandes"
+        />
+        <div>
+          <strong>
+            rocketseat/unform
+          </strong>
+          <p>
+            Performance-focused API for React forms rocket
+          </p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
+          alt="Diego Fernandes"
+        />
+        <div>
+          <strong>
+            rocketseat/unform
+          </strong>
+          <p>
+            Performance-focused API for React forms rocket
+          </p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
 );
 
 export default Dashboard;
